07a118cf854759cc19bb1d4e2ac9091d0f17d840
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-07-14 09:26:46 +0800

    float exit icons on wa

--- components/floatingIcon/index.js
+++ components/floatingIcon/index.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PiWhatsappLogo } from 'react-icons/pi'
 import { BsWhatsapp } from 'react-icons/bs'
-import {IoCloseCircleOutline} from 'react-icons/io'
+import { IoCloseCircleOutline } from "react-icons/io5";
 import styles from './styles.module.scss'
 import Link from 'next/link'
 export const FloatIcons = () => {
+
+    const [hidden, setHidden] = useState(false)
+
+    const handeClick = () => {
+        setHidden(true)
+    }
+
     return (
         <>
             <div className={styles.icons}>
-                <p>Kosultasi Sekarang!</p>
+                <div className={styles.icons__wrap}>
+                    {!hidden && (
+                        <>
+                            <IoCloseCircleOutline className={styles.icons__close} size={18} onClick={handeClick} />
+                            <div className={`${styles.icons__text} ${hidden ? "hide" : ""}`}>
+                                <p>
+                                    Kosultasi Sekarang!
+                                </p>
+                            </div>
+                        </>
+                    )}
+                </div>
+
                 <Link
                     className={styles.icons__link}
                     href={"#"}
